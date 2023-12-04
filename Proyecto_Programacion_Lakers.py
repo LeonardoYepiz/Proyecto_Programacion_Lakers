de4c9ece11515c7b5ef8f23d5f143308a552c91f
LeonardoYepiz <143147149+LeonardoYepiz@users.noreply.github.com>
2023-12-04 00:40:29 -0800

    Update Proyecto_Programacion_Lakers.py

--- Proyecto_Programacion_Lakers.py
+++ Proyecto_Programacion_Lakers.py
@@ -1,2 +1,23 @@
 #Proyecto Programacion 5to semestre
 #Web Scraping del equipo Los Angeles Lakers temporada 2022-2023
+
+from bs4 import BeautifulSoup
+import requests
+
+pagina=("https://www.basketball-reference.com/teams/LAL/2023.html")
+resultado=requests.get(pagina)
+contenido=resultado.text
+
+soup=BeautifulSoup(contenido, "lxml")
+#print(soup.prettify())
+titulo=soup.find("h1").get_text(strip= "True", separator= " ")
+print("El titulo de la pagina es el siguiente:", titulo)
+
+informacion = soup.find("div", {"id": "info"}).get_text(strip=True, separator=" ")
+
+
+informacion_por_renglones = informacion.split('\n')
+print("La informacion de la pagina principal es la siguiente:")
+for linea in informacion_por_renglones:
+    print(linea)
+
