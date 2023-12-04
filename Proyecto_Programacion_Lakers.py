f955dc17a26b93cfa74ade68335e51d5cf28adc2
LeonardoYepiz <143147149+LeonardoYepiz@users.noreply.github.com>
2023-12-04 15:41:31 -0800

    Update Proyecto_Programacion_Lakers.py

--- Proyecto_Programacion_Lakers.py
+++ Proyecto_Programacion_Lakers.py
@@ -1,7 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+from selenium import webdriver 
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
+#Webscraping manual - Obtencion de jugadores
 pagina = "https://www.basketball-reference.com/teams/LAL/2023.html"
 resultado = requests.get(pagina)
 contenido = resultado.text
@@ -24,17 +29,10 @@ for linea in informacion_por_renglones:
 jugadores_div = soup.find("div", {"id": "div_roster"})
 
 if jugadores_div:
-
     tabla = jugadores_div.find("table", {"class": "sortable"})
-
     if tabla:
-
         datos_jugadores = []
-
-
         filas = tabla.find_all("tr")
-
-
         for fila in filas[1:]:
             datos = fila.find_all(["th", "td"])
             jugador = [dato.get_text(strip=True) for dato in datos]
@@ -43,10 +41,45 @@ if jugadores_div:
 
         columnas = [header.get_text(strip=True) for header in filas[0].find_all("th")]
         data_df = pd.DataFrame(datos_jugadores, columns=columnas)
-
-
         print("La informacion de los jugadores es la siguiente:",data_df)
 
+#Webscraping automatizado - Obtencion de asitente y coaches
+s= Service(ChromeDriverManager().install())
+opc= Options()
+opc.add_argument("--window-size=1020,1200")
+navegador= webdriver.Chrome(service=s, options=opc)
+
+#Abrir la pagina
+url= "https://www.basketball-reference.com/teams/LAL/2023.html"
+navegador.get(url)
+
+#Encontrar la seccion de los asistentes y coaches del equipo
+asistentes_div= navegador.find_element(By.ID,"all_assistant_coaches")
+
+#Extraer la informacion de la seccion de asistentes
+
+if asistentes_div:
+    html= asistentes_div.get_attribute("innerHTML")
+    soup_asistentes= BeautifulSoup(html, "html.parser")
+
+    print("La informacion de los asistentes es la siguiente:")
+
+    #Procesar la informacion de los asistentes y entrenadores
+    data_asistentes = []
+    asistentes_rows = soup_asistentes.find_all("tr")
+    for row in asistentes_rows:
+        columns = row.find_all("td")
+        if columns:
+            nombre = columns[0].find("a").get_text(strip=True)
+            cargo = columns[1].get_text(strip=True)
+            data_asistentes.append({"Nombre": nombre, "Cargo": cargo})
+
+    # Crea el DataFrame con la información obtenida de asistentes
+    data_asistentes_df = pd.DataFrame(data_asistentes)
+    print(data_asistentes_df)  # Imprime el DataFrame de asistentes
+
+navegador.quit()
+    
 
 
 
