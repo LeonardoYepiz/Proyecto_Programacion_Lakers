d67e37976ee2a230762e0e18b47aa94c37d77f3f
LeonardoYepiz <143147149+LeonardoYepiz@users.noreply.github.com>
2023-12-04 14:02:25 -0800

    Update Proyecto_Programacion_Lakers.py

--- Proyecto_Programacion_Lakers.py
+++ Proyecto_Programacion_Lakers.py
@@ -3,6 +3,7 @@
 
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 pagina=("https://www.basketball-reference.com/teams/LAL/2023.html")
 resultado=requests.get(pagina)
@@ -20,4 +21,34 @@ informacion_por_renglones = informacion.split('\n')
 print("La informacion de la pagina principal es la siguiente:")
 for linea in informacion_por_renglones:
     print(linea)
+
+jugadores_div=soup.find("div", {"id":"div_roster"})
+
+if jugadores_div:
+    tabla=jugadores_div.find("table", {"class":"sortable"})
+
+    if tabla:
+        datos_jugadores=[]
+
+        filas=tabla.find_all("tr")
+
+        for fila in filas[1:]:
+            datos=fila.find_all(["th,"td])
+            jugador=[dato.get_text(strip=True) for dato in datos]
+
+        columnas=[header.get_text(strip=True) for header in filas [0].find_all("th")
+
+        data_df=pd.DataFrame(datos_jugadores,columns=columnas)
+        print("La informacion de los jugadores es la siguiente:", data_df)
+
+
+
+
+
+
+
+
+
+
+
 
